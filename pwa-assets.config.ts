ec455994f9fcd67be50ce9fb5b78c3c6e13560e3
Ларин Дмитрий <larin@tochka.com>
2026-05-11 10:28:44 +0500

    feat: Правка изображения для PWA

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig, minimal2023Preset } from '@vite-pwa/assets-generator/config'
 
 export default defineConfig({
-  preset: minimal2023Preset,
+  preset: {
+    ...minimal2023Preset,
+    apple: { ...minimal2023Preset.apple, padding: 0 },
+    transparent: { ...minimal2023Preset.transparent, padding: 0 },
+    maskable: { ...minimal2023Preset.maskable, padding: 0 },
+  },
   images: ['public/icon.svg'],
 })
